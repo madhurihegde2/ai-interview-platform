22a85a70b87d5a0b35b42e47e484e93ad53ee702
madhurihegde2 <madhurihegde2@gmail.com>
2026-07-30 12:01:35 -0700

    Implement login API with JWT authentication

--- ai-interview-platform/backend/src/config/prisma.js
+++ ai-interview-platform/backend/src/config/prisma.js
@@ -5,6 +5,7 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
+
 const prisma = new PrismaClient({
   adapter,
 });
--- ai-interview-platform/backend/src/controllers/authController.js
+++ ai-interview-platform/backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const prisma = require("../config/prisma");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
@@ -54,6 +55,69 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required.",
+      });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password.",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        message: "Invalid email or password.",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res.status(200).json({
+      message: "Login successful.",
+      token,
+      user: {
+        id: user.id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
